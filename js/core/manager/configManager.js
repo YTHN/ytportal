3867ffddc2566820a588515114ee26bedb797c83
EthanWang <ethan980301>
2017-01-10 19:07:03 +0800

    init

--- js/core/manager/configManager.js
+++ js/core/manager/configManager.js
@@ -2,7 +2,7 @@
  /*
 *created by: EthanWang
 *created at: 2017/1/10
-*description:配置管理器，通过获取不同来源的配置信息，实例化统一的ConfigData对象
+*description:配置管理器，通过获取不同来源的配置信息，实例化统一的ConfigData对象,这样通过BeX5服务返回的数据可以在这儿进行转换
 *
 *
 **/
